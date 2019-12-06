0182238b5b43f24e926053a24c7cd9eca5159c68
yivanusa <yivanusa@office.altertech.net>
2019-12-06 12:53:08 +0200

    fix parse func

--- pyaltt2/lp.py
+++ pyaltt2/lp.py
@@ -2,14 +2,13 @@ import re
 function_wrong_symbols = re.compile(r"[\ \"\'\:;_\/<>{}[\]~`]")
 
 
-def parse_func_str(val):
+def parse_func_str(val, auto_quote=True):
     """
     Parse value as function string
-
     e.g. myfunc(123, name="test')
-
     Args:
         val: value to parse
+        auto_quote: quotes wrapper
     Returns:
         tuple: function-name, tuple of args, dict of kwargs
     Raises:
@@ -32,12 +31,26 @@ def parse_func_str(val):
         if val.rsplit(')', 1)[1].strip(): raise ValueError
     except IndexError:
         raise ValueError
+    params = [v.replace(')', '') if v.endswith(')') else v for v in
+              val.split('(', 1)[1].split(',')]
+    new_params = []
+    if auto_quote:
+        for p in params:
+            new_p = p.split('=') if p.__contains__('=') else [None, p]
+            if new_p[1] and not re.match(r'"|\'\w*\"|\'', new_p[1].strip()):
+                    try:
+                        float(new_p[1])
+                    except ValueError:
+                        p = '"{}"'.format(new_p[1].strip()) if new_p[0] is None else '{}="{}"'.format(new_p[0], new_p[1].strip())
+            new_params.append(p)
+    else:
+        new_params = params
     code = textwrap.dedent("""
     def {}(*args, **kwargs):
         global a, kw
         a = args
         kw = kwargs
-    {}""").format(fname, val[len(pfx):])
+    {}""").format(fname, '{}({})'.format(fname, ', '.join(new_params)))
     d = {}
     try:
         exec(code, d)
